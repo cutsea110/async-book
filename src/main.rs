adf6798875871334f1fb65eb7979bcce198a2a4c
cutsea110 <cutsea110@gmail.com>
2021-06-13 13:09:25 +0900

    async

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use futures::join;
 use std::thread;
 
 fn download(_site: &str) {}
+
+async fn download_async(_site: &str) {}
 
 fn get_two_sites() {
     // Spawn two threads to do work.
@@ -12,8 +15,20 @@ fn get_two_sites() {
     thread_two.join().expect("thread two panicked");
 }
 
-fn main() {
+async fn get_two_sites_async() {
+    // Create two different "futures" which, when run to completion,
+    // will asynchronously download the webpages.
+    let future_one = download_async("https://www.foo.com");
+    let future_two = download_async("https://www.bar.com");
+
+    // Run both futures to completion at the same time.
+    join!(future_one, future_two);
+}
+
+#[async_std::main]
+async fn main() {
     println!("Hello, world!");
 
     get_two_sites();
+    get_two_sites_async().await;
 }
