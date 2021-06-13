550eec0396c8c980f14c98707604df7cea405ad9
cutsea110 <cutsea110@gmail.com>
2021-06-14 08:31:02 +0900

    hello-world

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,10 @@
-use futures::join;
-use std::thread;
+use futures::executor::block_on;
 
-fn download(_site: &str) {}
-
-async fn download_async(_site: &str) {}
-
-fn get_two_sites() {
-    // Spawn two threads to do work.
-    let thread_one = thread::spawn(|| download("https://www.foo.com"));
-    let thread_two = thread::spawn(|| download("https://www.bar.com"));
-
-    // Wait for both threads to complete.
-    thread_one.join().expect("thread one panicked");
-    thread_two.join().expect("thread two panicked");
+async fn hello_world() {
+    println!("hello, world!");
 }
 
-async fn get_two_sites_async() {
-    // Create two different "futures" which, when run to completion,
-    // will asynchronously download the webpages.
-    let future_one = download_async("https://www.foo.com");
-    let future_two = download_async("https://www.bar.com");
-
-    // Run both futures to completion at the same time.
-    join!(future_one, future_two);
-}
-
-#[async_std::main]
-async fn main() {
-    println!("Hello, world!");
-
-    get_two_sites();
-    get_two_sites_async().await;
+fn main() {
+    let future = hello_world(); // Nothing is printed
+    block_on(future); // `future` is run and "hello, world!" is printed
 }
